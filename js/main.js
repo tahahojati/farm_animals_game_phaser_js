63c8b82fd239af1d39ff608c07447026f2dd67d3
Taha Pourjalali <taha.hojati@gmail.com>
2018-01-23 16:39:39 -1000

    added images to the screen, adding clicklisteners to the arrows

--- js/main.js
+++ js/main.js
@@ -6,15 +6,50 @@ var GameState = {
         this.load.image('horse', 'assets/images/horse.png');
         this.load.image('pig', 'assets/images/pig.png');
         this.load.image('sheep', 'assets/images/sheep.png');
+        this.load.image('rightArrow', 'assets/images/right_arrow.png');
+        this.load.image('arrow', 'assets/images/arrow.png');
         
     },
     create: function(){
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; 
+        this.scale.pageAlignHorizontally = true; 
+        this.scale.pageAlignVertically = true; 
+        
+        this.rightArrow = this.game.add.sprite(500, this.game.world.centerY, 'arrow');
+        this.rightArrow.anchor.setTo(0.5); 
+        this.rightArrow.customParams = {direction: 1};
+        
+        this.rightArrow.inputEnabled = true; 
+        this.rightArrow.pixelPerfectClick = true; 
+        this.rightArrow.event.onInputDown.add(this.switchAnimal, this); 
+        
+        this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+        this.leftArrow.anchor.setTo(0.5); 
+        this.leftArrow.scale.x = -1 ; 
+        this.leftArrow.customParams = {direction: -1};
+        
+        //leftArrow allow user input
+        this.leftArrow.inputEnabled = true; 
+        this.leftArrow.pixelPerfectClick = true; 
+        this.leftArrow.event.onInputDown.add(this.SwitchAnimal, this); 
+        
+        
         this.background = this.game.add.sprite(0, 0, 'background');
         this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
         this.chicken.anchor.setTo(0.5, 0.5); 
-        
+        this.chicken.scale.setTo(2); 
+        this.sheep = this.game.add.sprite(100, 250, 'sheep');
+        this.sheep.scale.setTo(0.5);
+        this.sheep.anchor.setTo(0.5); 
+        this.sheep.angle = -45; 
     },
-    update: {},
+    update: function(){
+        this.sheep.angle += 0.5;
+    },
+    
+    switchAnimal: function(sprite, event){
+        console.log('move animal'); 
+    }
 };
 
 
